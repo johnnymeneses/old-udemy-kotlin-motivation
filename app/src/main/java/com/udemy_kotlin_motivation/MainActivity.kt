6e5de11e8235f7a2a0a3d46be2db9b55d34a139a
Johnny Meneses <bilbo_vader@outlook.com>
2022-04-09 19:24:33 -0300

    versao 1 da tela ok

--- app/src/main/java/com/udemy_kotlin_motivation/MainActivity.kt
+++ app/src/main/java/com/udemy_kotlin_motivation/MainActivity.kt
@@ -2,6 +2,7 @@ package com.udemy_kotlin_motivation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,6 +10,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         supportActionBar!!.hide()
+
+
+        btn_novaFrase.setOnClickListener(){
+            println("CLIQUEI")
+        }
+
+
+
+
+
+
 
     }
 }
