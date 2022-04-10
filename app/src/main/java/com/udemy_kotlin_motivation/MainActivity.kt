107a8a6e54664bb62d4617dd74b30686ff10166d
Johnny Meneses <bilbo_vader@outlook.com>
2022-04-09 21:43:21 -0300

    Splash Activity

--- app/src/main/java/com/udemy_kotlin_motivation/MainActivity.kt
+++ app/src/main/java/com/udemy_kotlin_motivation/MainActivity.kt
@@ -2,25 +2,34 @@ package com.udemy_kotlin_motivation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.udemy_kotlin_motivation.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+
+    //variavel que vai receber o biding
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportActionBar!!.hide()
+        //Esconder barra de navegação
+        supportActionBar?.hide()
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        btn_novaFrase.setOnClickListener(){
-            println("CLIQUEI")
+        binding.btnNovaFrase.setOnClickListener(this)
+
+    }
+
+    //Contrato View.OnClickListener assinado, implementar membros
+    override fun onClick(view: View) {
+        if (view.id == R.id.btn_novaFrase) {
+            println("Cliquei")
         }
-
-
-
-
-
-
-
     }
 }
---
+++ app/src/main/java/com/udemy_kotlin_motivation/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.udemy_kotlin_motivation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        
+        supportActionBar?.hide()
+
+
+
+    }
+}
